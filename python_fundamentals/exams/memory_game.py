9310bde18c5b5d38cb79b57fd476b84183e13d46
ivaylo borisov <ivoplayftw737@gmail.com>
2022-10-21 01:35:22 +0300

    save

--- python_fundamentals/exams/memory_game.py
+++ python_fundamentals/exams/memory_game.py
@@ -4,14 +4,15 @@ moves = 0
 
 while True:
     command = input()
-    splitted = command.split()
+    splitted = [int(ele) for ele in command.split()]
     if command == "end":
         break
     if splitted[0] == splitted[1] or \
             splitted[0] < 0 or splitted[1] < 0 \
-            or splitted > len(new_sequence):
+            or splitted[0] > len(new_sequence) \
+            or splitted[1] > len(new_sequence):
         moves += 1
-        new_ele = -moves + 'a'
+        new_ele = str(-moves) + 'a'
         mid_of_seq = len(new_sequence) // 2
-        splitted.insert(mid_of_seq, new_ele)
+        new_sequence.insert(mid_of_seq, new_ele)
         print("Invalid input! Adding additional elements to the board")
