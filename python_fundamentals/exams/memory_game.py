f0f71ff480859eb458f838da7f01b8ea5dc212cf
ivaylo borisov <ivoplayftw737@gmail.com>
2022-10-21 12:23:24 +0300

    new exercise

--- python_fundamentals/exams/memory_game.py
+++ python_fundamentals/exams/memory_game.py
@@ -1,18 +1,36 @@
 sequence = input().split()
-new_sequence = [sequence]
 moves = 0
-
 while True:
-    command = input()
-    splitted = [int(ele) for ele in command.split()]
-    if command == "end":
+    command = input().split()
+
+    if command[0] == "end":
+        if len(sequence) > 0:
+            print("Sorry you lose :(")
+            print(*sequence)
+            break
         break
+    splitted = [int(num) for num in command]
     if splitted[0] == splitted[1] or \
             splitted[0] < 0 or splitted[1] < 0 \
-            or splitted[0] > len(new_sequence) \
-            or splitted[1] > len(new_sequence):
+            or splitted[0] > len(sequence) - 1 \
+            or splitted[1] > len(sequence) - 1:
         moves += 1
-        new_ele = str(-moves) + 'a'
-        mid_of_seq = len(new_sequence) // 2
-        new_sequence.insert(mid_of_seq, new_ele)
+        new_ele = f"-{moves}a"
+        mid_of_seq = len(sequence) // 2
+        sequence.insert(mid_of_seq, new_ele)
+        sequence.insert(mid_of_seq, new_ele)
         print("Invalid input! Adding additional elements to the board")
+        continue
+    if sequence[splitted[0]] == sequence[splitted[1]]:
+        moves += 1
+        found_ele = sequence[splitted[0]]
+        for ele in range(len(sequence)):
+            if found_ele in sequence:
+                sequence.remove(found_ele)
+        print(f"Congrats! You have found matching elements - {found_ele}!")
+    elif sequence[splitted[0]] != sequence[splitted[1]]:
+        moves += 1
+        print("Try again!")
+    if len(sequence) == 0 and command[0] != 'end':
+        print(f"You have won in {moves} turns!")
+        break
