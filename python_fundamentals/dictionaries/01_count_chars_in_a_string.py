0423f4501a3b4d5908d287e053bc1f0b2f582c8c
ivoplayftw <ivoplayftw737@gmail.com>
2022-11-11 13:24:49 +0200

    added new problem

--- python_fundamentals/dictionaries/01_count_chars_in_a_string.py
+++ python_fundamentals/dictionaries/01_count_chars_in_a_string.py
@@ -1,6 +1,10 @@
-text = input().spit()
+text = input().split()
 my_dict = {}
-symbols = ''
-for ele in text:
-    symbols += ele
+symbols = ''.join(text)
+for symbol in symbols:
+    if symbol not in my_dict.keys():
+        my_dict[symbol] = 0
+    my_dict[symbol] += 1
 
+for char, num in my_dict.items():
+    print(f"{char} -> {num}")
