6fea3716f823d5965559d2de4135fc9d4d42a1f5
ivoplayftw <ivoplayftw737@gmail.com>
2022-11-12 12:28:46 +0200

    updated problem

--- python_fundamentals/dictionaries/02_a_miners_task.py
+++ python_fundamentals/dictionaries/02_a_miners_task.py
@@ -1,17 +1,25 @@
 resources = {}
 counter = 0
+last_resource = ''
 while True:
     command = input()
     if command == "stop":
         break
+
     counter += 1
+
     if counter % 2 != 0:
+        if command in resources.keys():
+            last_resource = command
+            continue
         resources[command] = 0
+
     elif counter % 2 == 0:
         for keys in resources.keys():
             if resources[keys] == 0:
-                resources[keys] = command
+                resources[keys] = int(command)
+        if last_resource in resources.keys():
+            resources[last_resource] += int(command)
 
-    last_command = command
 for key, value in resources.items():
     print(f"{key} -> {value}")
