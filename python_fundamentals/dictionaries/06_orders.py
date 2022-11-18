fbd97bc92af8a5f1f364093b9a5784e147d1727b
ivoplayftw <ivoplayftw737@gmail.com>
2022-11-18 20:18:19 +0200

    updated problem

--- python_fundamentals/dictionaries/06_orders.py
+++ python_fundamentals/dictionaries/06_orders.py
@@ -1,15 +1,20 @@
 command = input()
 products_and_price = {}
+products_quantity = {}
+products_full_price = {}
 quantity = []
 while command != 'buy':
     splitted = command.split()
-    for i in range(0, len(splitted), 2):
-        quantity.append[splitted[i]]
-    if splitted[0] in products_and_price:
+    if splitted[0] in products_and_price and splitted[0] in products_quantity:
         products_and_price[splitted[0]] = float(splitted[1])
+        products_quantity[splitted[0]] += int(splitted[2])
+        products_full_price[splitted[0]] = products_quantity[splitted[0]] * float(splitted[1])
     else:
         products_and_price[splitted[0]] = float(splitted[1])
+        products_quantity[splitted[0]] = int(splitted[2])
+        products_full_price[splitted[0]] = int(splitted[2]) * float(splitted[1])
 
     command = input()
 
-for product, price in products_and_price.items():
+for key, value in products_full_price.items():
+    print(f"{key} -> {value:.2f}")
